c3e37779ead9740d395efd16cb641953da301118
drewmps <andrewmarco97@gmail.com>
2025-04-18 16:37:39 +0700

    feat: Register Screen - functionality register

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,13 +1,56 @@
+import { gql, useMutation } from "@apollo/client";
+import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
 import { Text, TextInput, View, StyleSheet, Button } from "react-native";
 
+const REGISTER = gql`
+  mutation RegisterUser($newUser: UserInput) {
+    registerUser(newUser: $newUser)
+  }
+`;
+
 export default function RegisterScreen() {
+  const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const navigation = useNavigation();
 
+  const [handleRegister, { data, loading, error }] = useMutation(REGISTER, {
+    onCompleted: (result) => {
+      navigation.navigate("LoginScreen");
+    },
+  });
+
+  const onSubmit = () => {
+    const payload = {
+      email,
+      name,
+      password,
+      username,
+    };
+    handleRegister({
+      variables: {
+        newUser: payload,
+      },
+    });
+  };
   return (
     <View>
       <Text>RegisterScreen</Text>
+      <Text>Name</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setName(text)}
+        value={name}
+        secureTextEntry={true}
+      />
+      <Text>Email</Text>
+      <TextInput
+        style={styles.input}
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
       <Text>Username</Text>
       <TextInput
         style={styles.input}
@@ -21,7 +64,7 @@ export default function RegisterScreen() {
         value={password}
         secureTextEntry={true}
       />
-      <Button title="Register" />
+      <Button title="Register" onPress={onSubmit} />
     </View>
   );
 }
